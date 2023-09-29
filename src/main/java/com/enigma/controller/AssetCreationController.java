4227c18d3ddb28b0e97d98ac1004ff2e79011ebd
BimalMurali <bimalmurali999@gmail.com>
2023-09-29 12:13:25 +0530

    Added enigma.common package and application.properties

---
+++ src/main/java/com/enigma/controller/AssetCreationController.java
@@ -0,0 +1,53 @@
+package com.enigma.controller;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enigma.common.APIResponse;
+import com.enigma.entity.AssetCreation;
+import com.enigma.services.IAssetCreationService;
+
+@CrossOrigin
+@RestController //@Controller+@Configuration
+@RequestMapping("/api")
+public class AssetCreationController {
+	@Autowired
+	private IAssetCreationService assetService;
+	@Autowired
+	private APIResponse apiResponse;
+
+	
+	
+	    //list
+		@GetMapping("/asset_creation")
+		public List<AssetCreation> getAssetDefinition(@RequestHeader(value="authorization",defaultValue="")String auth) throws AccessDeniedException{
+			return assetService.getAssetDefinition();
+		}
+			
+		//add
+		@PostMapping("/asset_creation")
+		public ResponseEntity<APIResponse> addAssetCreation(@RequestBody AssetCreation asset){
+			if(assetService.saveAsset(asset)==null) {
+				apiResponse.setData(("Name can have only alphabets"));
+				apiResponse.setStatus(500);
+				apiResponse.setError("Invalid name");
+				return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
+			}
+			apiResponse.setData("Asset added sucessfully");
+			apiResponse.setStatus(200);
+			return ResponseEntity.status(apiResponse.getStatus()).body(apiResponse);
+				
+		}
+
+}
+
--- src/main/java/com/enigma/services/AssetCreationServiceImple.java
+++ src/main/java/com/enigma/services/AssetCreationServiceImple.java
@@ -5,17 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.enigma.entity.AssetDefinition;
+import com.enigma.entity.AssetCreation;
 import com.enigma.repository.IAssetDefRepository;
 @Service
-public class AssetDefServiceImple implements IAssetDefService {
+public class AssetCreationServiceImple implements IAssetCreationService {
 	@Autowired
 	private IAssetDefRepository assetRepo;
 
 	
-	public List<AssetDefinition> getAssetDefinition() {
+	public List<AssetCreation> getAssetDefinition() {
 		// TODO Auto-generated method stub
-		return (List<AssetDefinition>) assetRepo.findAll();
+		return (List<AssetCreation>) assetRepo.findAll();
+	}
+
+
+	public AssetCreation saveAsset(AssetCreation asset) {
+		return assetRepo.save(asset);
 	}
 
 }
