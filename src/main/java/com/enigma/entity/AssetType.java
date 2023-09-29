63542367f6cda5d99d8c6ae8c9547e573e237b95
anjanac <anjanac.janardhanan@gmail.com>
2023-09-29 16:53:19 +0530

    ApplicationProperties Changed

--- src/main/java/com/enigma/entity/AssetType.java
+++ src/main/java/com/enigma/entity/AssetType.java
@@ -1,5 +1,5 @@
-package com.enigma.entity;
-
-public class AssetType {
-
-}
+//package com.enigma.entity;
+//
+//public class AssetType {
+//
+//}
--- src/main/java/com/enigma/repository/IVendorCreationRepository.java
+++ src/main/java/com/enigma/repository/IVendorCreationRepository.java
@@ -1,6 +1,9 @@
 package com.enigma.repository;
 
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.enigma.entity.VendorCreation;
@@ -11,8 +14,8 @@ import com.enigma.entity.VendorCreation;
 
 
 public interface IVendorCreationRepository extends CrudRepository<VendorCreation,Integer>{
-	//@Query("from VendorCreation WHERE vendorName like %?1%")
-	//public List<VendorCreation> findByVendorByName(String name);
+	@Query("from VendorCreation WHERE vendorName like %?1%")
+	public List<VendorCreation> findByVendorByName(String name);
 
 	//public List<VendorCreation> getVendorByName(String name);
 
