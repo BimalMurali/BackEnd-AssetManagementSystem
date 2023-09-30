0b9b572af297a24c9e3fafa3006ec39f0577313f
PriyankaRoy <roypriy16@gmail.com>
2023-09-30 12:17:25 +0530

    edited mappings

--- src/main/java/com/enigma/entity/PurchaseOrder.java
+++ src/main/java/com/enigma/entity/PurchaseOrder.java
@@ -25,10 +25,9 @@ public class PurchaseOrder {
 	private String purchaseOrderNo;
 
 	
-	private int assetTypeId;
-	@OneToOne
-	@JoinColumn(name="assetTypeId",nullable=false,updatable=false)
-	private AssetType assetType;
+	@ManyToOne
+    @JoinColumn(name = "assetTypeId", nullable = false, updatable = false)
+    private AssetType assetType;
 	
 	
 	@Column(name="AssetId")
@@ -74,17 +73,19 @@ public class PurchaseOrder {
 		this.assetdefinition = assetdefinition;
 	}
 
+
 	public void setAssetType(AssetType assetType) {
 		this.assetType = assetType;
 	}
 
-	public PurchaseOrder(Integer id, String purchaseOrderNo, int assetTypeId, AssetType assetType, int assetId,
+	
+
+	public PurchaseOrder(Integer id, String purchaseOrderNo, AssetType assetType, int assetId,
 			AssetDefinition assetdefinition, Integer vendorId, VendorCreation vendorCreation, Date orderDate,
 			Date deliveryDate, Integer statusId, Status status) {
 		super();
 		this.id = id;
 		this.purchaseOrderNo = purchaseOrderNo;
-		this.assetTypeId = assetTypeId;
 		this.assetType = assetType;
 		this.assetId = assetId;
 		this.assetdefinition = assetdefinition;
@@ -104,13 +105,7 @@ public class PurchaseOrder {
 		this.purchaseOrderNo = purchaseOrderNo;
 	}
 
-	public int getAssetTypeId() {
-		return assetTypeId;
-	}
 
-	public void setAssetTypeId(int assetTypeId) {
-		this.assetTypeId = assetTypeId;
-	}
 
 	public Integer getVendorId() {
 		return vendorId;
@@ -156,13 +151,6 @@ public class PurchaseOrder {
 		this.purchaseOrderNo = number;
 	}
 
-	public int getAssetType() {
-		return assetTypeId;
-	}
-
-	public void setAssetType(int assetType) {
-		this.assetTypeId = assetType;
-	}
 
 
 	public Date getOrderDate() {
@@ -191,19 +179,19 @@ public class PurchaseOrder {
 
 	@Override
 	public String toString() {
-		return "PurchaseOrder [id=" + id + ", purchaseOrderNo=" + purchaseOrderNo + ", assetTypeId=" + assetTypeId
-				+ ", assetType=" + assetType + ", assetId=" + assetId + ", assetdefinition=" + assetdefinition
-				+ ", vendorId=" + vendorId + ", vendorCreation=" + vendorCreation + ", orderDate=" + orderDate
-				+ ", deliveryDate=" + deliveryDate + ", statusId=" + statusId + ", status=" + status + ", getAssetId()="
-				+ getAssetId() + ", getAssetdefinition()=" + getAssetdefinition() + ", getPurchaseOrderNo()="
-				+ getPurchaseOrderNo() + ", getAssetTypeId()=" + getAssetTypeId() + ", getVendorId()=" + getVendorId()
-				+ ", getVendorCreation()=" + getVendorCreation() + ", getStatus()=" + getStatus() + ", getId()="
-				+ getId() + ", getNumber()=" + getNumber() + ", getAssetType()=" + getAssetType() + ", getOrderDate()="
+		return "PurchaseOrder [id=" + id + ", purchaseOrderNo=" + purchaseOrderNo + ", assetType=" + assetType
+				+ ", assetId=" + assetId + ", assetdefinition=" + assetdefinition + ", vendorId=" + vendorId
+				+ ", vendorCreation=" + vendorCreation + ", orderDate=" + orderDate + ", deliveryDate=" + deliveryDate
+				+ ", statusId=" + statusId + ", status=" + status + ", getAssetId()=" + getAssetId()
+				+ ", getAssetdefinition()=" + getAssetdefinition() + ", getPurchaseOrderNo()=" + getPurchaseOrderNo()
+				+ ", getVendorId()=" + getVendorId() + ", getVendorCreation()=" + getVendorCreation() + ", getStatus()="
+				+ getStatus() + ", getId()=" + getId() + ", getNumber()=" + getNumber() + ", getOrderDate()="
 				+ getOrderDate() + ", getDeliveryDate()=" + getDeliveryDate() + ", getStatusId()=" + getStatusId()
 				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
 				+ "]";
 	}
 
+	
 
 
 }
