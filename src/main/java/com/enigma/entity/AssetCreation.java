8eb0e2b7c17e9ae4050875fb5644c8655423407b
PriyankaRoy <roypriy16@gmail.com>
2023-10-04 19:01:38 +0530

    added

--- src/main/java/com/enigma/entity/AssetCreation.java
+++ src/main/java/com/enigma/entity/AssetCreation.java
@@ -37,10 +37,10 @@ public class AssetCreation {
 	@JoinColumn(name="modelId",insertable=false,updatable=false)
 	private AssetDefinition assetdef;
 
-	private int assetTypeId;
-	@OneToOne
-	@JoinColumn(name="assetTypeId",insertable=false,updatable=false)
-	private AssetType assettypedef;
+//	private int assetTypeId;
+//	@OneToOne
+//	@JoinColumn(name="assetTypeId",insertable=false,updatable=false)
+//	private AssetType assettypedef;
 	
 	
 	@Column(name="serialNo",nullable=false,length=60)
@@ -111,21 +111,21 @@ public class AssetCreation {
 		this.assetdef = assetdef;
 	}
 
-	public int getAssetTypeId() {
-		return assetTypeId;
-	}
-
-	public void setAssetTypeId(int assetTypeId) {
-		this.assetTypeId = assetTypeId;
-	}
-
-	public AssetType getAssettypedef() {
-		return assettypedef;
-	}
-
-	public void setAssettypedef(AssetType assettypedef) {
-		this.assettypedef = assettypedef;
-	}
+//	public int getAssetTypeId() {
+//		return assetTypeId;
+//	}
+//
+//	public void setAssetTypeId(int assetTypeId) {
+//		this.assetTypeId = assetTypeId;
+//	}
+//
+//	public AssetType getAssettypedef() {
+//		return assettypedef;
+//	}
+//
+//	public void setAssettypedef(AssetType assettypedef) {
+//		this.assettypedef = assettypedef;
+	//}
 
 	public String getSerialNo() {
 		return serialNo;
@@ -176,7 +176,7 @@ public class AssetCreation {
 	}
 
 	public AssetCreation(Integer id, Integer vendorAssetTypeID, VendorCreation vendors, String make, int modelId,
-			AssetDefinition assetdef, int assetTypeId, AssetType assettypedef, String serialNo,
+			AssetDefinition assetdef, String serialNo,
 			String yearOfManufacture, Date purchaseDate, String warrranty, Date fromDate, Date toDate) {
 		super();
 		this.id = id;
@@ -185,8 +185,8 @@ public class AssetCreation {
 		this.make = make;
 		this.modelId = modelId;
 		this.assetdef = assetdef;
-		this.assetTypeId = assetTypeId;
-		this.assettypedef = assettypedef;
+//		this.assetTypeId = assetTypeId;
+//		this.assettypedef = assettypedef;
 		this.serialNo = serialNo;
 		this.yearOfManufacture = yearOfManufacture;
 		this.purchaseDate = purchaseDate;
@@ -202,20 +202,18 @@ public class AssetCreation {
 	@Override
 	public String toString() {
 		return "AssetCreation [id=" + id + ", VendorAssetTypeID=" + VendorAssetTypeID + ", vendors=" + vendors
-				+ ", make=" + make + ", modelId=" + modelId + ", assetdef=" + assetdef + ", assetTypeId=" + assetTypeId
-				+ ", assettypedef=" + assettypedef + ", serialNo=" + serialNo + ", yearOfManufacture="
-				+ yearOfManufacture + ", purchaseDate=" + purchaseDate + ", warrranty=" + warrranty + ", fromDate="
-				+ fromDate + ", toDate=" + toDate + "]";
+				+ ", make=" + make + ", modelId=" + modelId + ", assetdef=" + assetdef + ", serialNo=" + serialNo
+				+ ", yearOfManufacture=" + yearOfManufacture + ", purchaseDate=" + purchaseDate + ", warrranty="
+				+ warrranty + ", fromDate=" + fromDate + ", toDate=" + toDate + ", getId()=" + getId()
+				+ ", getVendorAssetTypeID()=" + getVendorAssetTypeID() + ", getVendors()=" + getVendors()
+				+ ", getMake()=" + getMake() + ", getModelId()=" + getModelId() + ", getAssetdef()=" + getAssetdef()
+				+ ", getSerialNo()=" + getSerialNo() + ", getYearOfManufacture()=" + getYearOfManufacture()
+				+ ", getPurchaseDate()=" + getPurchaseDate() + ", getWarrranty()=" + getWarrranty() + ", getFromDate()="
+				+ getFromDate() + ", getToDate()=" + getToDate() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
 	}
 
 	
 	
-	
-
-	
-
-	
-	
-
 	
 }
